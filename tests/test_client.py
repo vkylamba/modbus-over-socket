7f943ea06cbccc40e9f983aa68374593d94e8484
vkylamba <vikas13jun@gmail.com>
2020-10-05 17:36:32 +0200

    added docker-compose

---
+++ tests/test_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '173.255.251.218'  # Standard loopback interface address (localhost)
+PORT = 8024        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Heartbeat ooo')
+    data = s.recv(1024)
+
+print('Received', repr(data))
