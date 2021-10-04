fad6fd1c0edf2166235f92a6d972d6bfe4d829c0
vkylamba <vikas13jun@gmail.com>
2021-10-04 09:45:13 +0200

    log format fix

--- loggers/console_logger.py
+++ loggers/console_logger.py
@@ -1,15 +1,16 @@
 import sys
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 def get_console_handler():
     console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
     return console_handler
 
 def get_file_handler():
     file_handler = logging.FileHandler("logs.txt")
+    file_handler.setFormatter(formatter)
     return file_handler
 
 
