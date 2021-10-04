fc970529e52a2fe930a67adbb1d6a5b07d864ae1
vkylamba <vikas13jun@gmail.com>
2021-10-04 09:37:04 +0200

    logging fix

--- socket_server.py
+++ socket_server.py
@@ -10,15 +10,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the address given on the command line
 server_name = sys.argv[1]
 server_address = (server_name, 8024)
-logger.info(sys.stderr, 'starting up on %s port %s' % server_address)
+logger.info('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 sock.listen(5)
 
 while True:
-    logger.info(sys.stderr, 'waiting for a connection')
+    logger.info('waiting for a connection')
     connection, client_address = sock.accept()
     try:
-        logger.info(sys.stderr, 'client connected:', client_address)
+        logger.info('client connected:', client_address)
         client_handler = ClientHandler(
             connection,
             client_address
