bb560e97f3ab3df8431ce3353583668296bdd619
vkylamba <vikas13jun@gmail.com>
2024-10-26 11:22:13 +0200

    Fixed bug

--- loggers/postgres_logger.py
+++ loggers/postgres_logger.py
@@ -6,33 +6,32 @@ DATABASE_NAME = os.environ.get('DATABASE_NAME', 'iotmodbus')
 DATABASE_USER = os.environ.get('DATABASE_USER', '')
 DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', '')
 
-con = None
 curs_obj = None
 
 def init_connection():
-    if con is None or curs_obj is None:
-        con = psycopg2.connect(
-            database=DATABASE_NAME,
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD,
-            host=DATABASE_HOST,
-            port= '5432'
-        )
-        curs_obj = con.cursor()
+    con = psycopg2.connect(
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        port= '5432'
+    )
+    curs_obj = con.cursor()
+    return curs_obj
 
 
 def log_to_db(device, key_name, register_address, value):
-    # create table modbusdata (deviceId varchar(255), time timestamp, key varchar(255), register varchar(255), value float);
+    # create table modbusdata (deviceId varchar(255), time timestamp, key varchar(255), register varchar(255), value varchar(255));
     try:
-        init_connection()
-        curs_obj.execute("""
-            INSERT INTO modbusdata(time, key, register, value)
-            VALUES('2024-10-10T00:00:00Z', "voltage", 3434, 345.66);
+        if curs_obj is None:
+            curs_obj = init_connection()
+        curs_obj.execute(f"""
+            INSERT INTO modbusdata(deviceId, time, key, register, value)
+            VALUES({device}, {str(key_name)}, {str(register_address)}, {str(value)});
         """)
     except Exception as ex:
         print("log_to_db error")
         print(ex)
-        con = None
         curs_obj = None
 
 
